5d6db4803a978d659db5fc3309a63a49591ff5f2
qinjie <qinjie@fulan.com.cn>
2020-09-09 17:59:06 +0800

    pet-api-base

---
+++ pet-api-base/src/main/java/com/ecnu/edu/petapibase/examplea/domain/UserDO.java
@@ -0,0 +1,13 @@
+package com.ecnu.edu.petapibase.examplea.domain;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @author 13862
+ */
+@Data
+public class UserDO implements Serializable {
+
+}
