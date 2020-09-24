1bccbb15495beea46cae397e61d5f71bb2761de8
qinjie <qinjie@fulan.com.cn>
2020-09-24 13:30:26 +0800

    sso模块

---
+++ pet-sso/src/main/java/com/ecnu/edu/petsso/PetSsoApplication.java
@@ -0,0 +1,13 @@
+package com.ecnu.edu.petsso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetSsoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetSsoApplication.class, args);
+    }
+
+}
---
+++ pet-sso/src/test/java/com/ecnu/edu/petsso/PetSsoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecnu.edu.petsso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetSsoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
