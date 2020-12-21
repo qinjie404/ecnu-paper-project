2fa3341c6734e809675d78a43651d16db79d6460
qinjie <qinjie@fulan.com.cn>
2020-12-21 17:39:17 +0800

    pet-auth

---
+++ pet-auth/src/test/java/com/ecnu/edu/petauth/PetAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecnu.edu.petauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
