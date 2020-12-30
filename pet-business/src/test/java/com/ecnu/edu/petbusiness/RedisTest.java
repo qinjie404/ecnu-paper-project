7550f75dd41a2bdd8ae38ace4075fa7a597b285b
qinjie <qinjie@fulan.com.cn>
2020-12-30 17:35:43 +0800

    整合redis

---
+++ pet-business/src/test/java/com/ecnu/edu/petbusiness/RedisTest.java
@@ -0,0 +1,39 @@
+package com.ecnu.edu.petbusiness;
+
+import com.ecnu.edu.petapibase.base.util.RedisUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author 13862
+ * @description
+ * @date 2020/12/30 16:31
+ */
+@SpringBootTest
+@Slf4j
+@Import(PetBusinessApplication.class)
+public class RedisTest {
+
+    @Autowired
+    private RedisUtil redisUtil;
+
+    @Test
+    void hsetTest() {
+        redisUtil.hset("qinjie", "1", 1);
+        redisUtil.hset("qinjie", "2", 2);
+    }
+    @Test
+    void hmsetTest() {
+        Map<String,Object> map=new HashMap<>();
+        map.put("a", 1);
+        map.put("b", 1);
+        redisUtil.hmset("qq", map,60);
+
+    }
+}
